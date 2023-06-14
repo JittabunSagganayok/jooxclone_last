0380818b673d397eea495f4801d580410304a99f
Jittabun Sagganayok <jitaboon1@hotmail.com>
2023-06-14 18:02:21 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:jooxclone_jittabun/views/audioplayer_screen.dart';
+import 'package:jooxclone_jittabun/views/login_page.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:rxdart/rxdart.dart';
@@ -36,8 +37,8 @@ class MyApp extends StatelessWidget {
         //   unselectedItemColor: Colors.white38,
         // ),
       ),
-      home: const AudioPlayerScreen(),
-      //home: LoginPage(),
+      //home: const AudioPlayerScreen(),
+      home: LoginPage(),
     );
   }
 }
